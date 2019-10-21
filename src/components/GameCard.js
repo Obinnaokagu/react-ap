913132f5527e27025f8ca1fdd1003d675179bfd7
Johnson Okagu <obyno4reel@gmail.com>
2019-10-21 13:02:31 +0100

    making components reusable with props

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-const GameCard = () => (
+const GameCard = ({ game }) => (
   <div className="ui card">
     <div className="image">
-      <span className="ui green ribbon label">$35.99</span> 
-      <img src="https://cf.geekdo-images.com/W9bbXs5FZwzLNlT5Pz6G-uV2Wek=/fit-in/1200x630/pic3251870.jpg" alt="GameCover" />
+      <span className="ui green ribbon label">${game.price}</span> 
+      <img src={game.thumbnail} alt="GameCover" />
     </div>
     <div className="content">
-        <a href="#" className='header'>Quadropolis</a>
+        <a href="#" className='header'>{game.name}</a>
         <div className='meta'>
-            <i className='icon users'></i> 2-4&nbsp;
-            <i className='icon wait'></i> 60 min.
+            <i className='icon users'></i> {game.players}&nbsp;
+            <i className='icon wait'></i> {game.duration} min.
         </div>
     </div>
   </div>
