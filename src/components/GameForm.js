bd1de0b30d2a370bad14fdfc1614d28ce3b0575a
Johnson Okagu <obyno4reel@gmail.com>
2019-10-27 16:49:03 +0100

    checkboxes

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 
+const tags = [
+  { _id: 1, name: 'dice' },
+  { _id: 2, name: 'economic' },
+  { _id: 3, name: 'family' },
+  { _id: 4, name: 'children' },
+];
+
 class GameForm extends Component {
   state = {
     name: '',
@@ -7,14 +14,22 @@ class GameForm extends Component {
     price: 0,
     duration: 0,
     players: '',
+    featured: false,
+    tags: [],
   };
   handleSumbit = e => {
     e.preventDefault();
     console.log(this.state);
   };
-  handleStringChange = e =>
-    this.setState({ [e.target.name]: e.target.value });
-    handleNumberChange = e => this.setState({ [e.target.name]: parseInt(e.target.value, 10) });
+  handleStringChange = e => this.setState({ [e.target.name]: e.target.value });
+  handleNumberChange = e =>
+    this.setState({ [e.target.name]: parseInt(e.target.value, 10) });
+  handleCheckboxChange = e =>
+    this.setState({ [e.target.name]: e.target.checked });
+  toggleTag = tag =>
+    this.state.tags.includes(tag._id)
+      ? this.setState({ tags: this.state.tags.filter(id => id !== tag._id) })
+      : this.setState({ tags: [...this.state.tags, tag._id] });
 
   render() {
     return (
@@ -75,7 +90,31 @@ class GameForm extends Component {
             />
           </div>
         </div>
-        
+        <div className="inline field">
+          <input
+            id="featured"
+            name="featured"
+            type="checkbox"
+            checked={this.state.featured}
+            onChange={this.handleCheckboxChange}
+          />
+          <label htmlFor="faetured">featured?</label>
+        </div>
+        <div className="field">
+          <label>tags</label>
+          {tags.map(tag => (
+            <div key={tag._id} className="inline field">
+              <input
+                id={`tag-${tag._id}`}
+                type="checkbox"
+                checked={this.state.tags.includes(tag._id)}
+                onChange={() => this.toggleTag(tag)}
+              />
+              <label htmlFor={`tag-${tag._id}`}>{tag.name}</label>
+            </div>
+          ))}
+        </div>
+
         <button className="ui button" type="submit">
           Create
         </button>
