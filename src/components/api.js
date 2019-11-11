667853740d255e11bfda9b57136174965b7a7f6c
Johnson Okagu <obyno4reel@gmail.com>
2019-11-11 16:21:56 +0100

    trying to correct the error in my app

--- src/components/api.js
+++ src/components/api.js
@@ -1,6 +1,9 @@
-import axios from "axios";
-export default {
-    games: {
-        fetchAll: () => axios.get("/api/unsafegames").then(res => res.data.games)
-    }
-};
+// import axios from "axios";
+ 
+// export default {
+//     games: {
+//         fetchAll: () => axios.get("/api/unsafegames").then(res => res.data.games),
+//         create: game =>
+//         axios.post("/api/unsafegames", { game }).then(res => res.data.game)
+//     }
+// };
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import GameCard from './components/GameCard';
-import App from './components/App';
-
-import 'semantic-ui-css/semantic.min.css';
-
-render(<App />, document.getElementById('root'));
- 
